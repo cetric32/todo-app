b9a2658cb9d4f39d8631e24bce2dd58494476b7c
cetric <cetric32@gmail.com>
2020-01-01 11:10:43 +0300

    todo app in vuejs
     Changes to be committed:
    	modified:   src/App.vue
    	new file:   src/components/AddTodo.vue
    	modified:   src/components/HelloWorld.vue
    	new file:   src/components/TodoItem.vue
    	new file:   src/components/Todos.vue
    	new file:   src/components/layout/Header.vue

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+      <form @submit.prevent="addTodo">
+          <input type="text" 
+          name="title"
+          placeholder="Add Todo..."
+          v-model="title">
+          <input type="submit" value="Submit" class="btn">
+      </form>
+  </div>
+</template>
+
+<script>
+import uuid from 'uuid';
+
+export default {
+    name:'AddTodo',
+    data(){
+        return {
+            title: null 
+        }
+    },
+    methods: {
+        addTodo(e){
+            e.preventDefault();
+            const newTodo = {
+                id: uuid.v4(),
+                title: this.title,
+                completed: false
+            }
+            //reset title
+            this.title = null;
+            //send up to parent
+            this.$emit('add-todo', newTodo);
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+    <h1>Todos</h1>
+   
+        <div v-for="(todo, index) in todos" :key="index">
+            <Todo-item 
+            :todo='todo'
+            @del-todo="$emit('del-todo',todo.id)"
+            ></Todo-item>
+        </div>
+</div>  
+</template>
+
+<script>
+import TodoItem from './TodoItem'
+
+export default {
+    name: "Todos",
+    props:{
+        todos:Array
+    },
+    components:{
+        TodoItem
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
